707d7cd9b9a10c19df791a1390ef2b5929641634
Mrpaker <spectator_w@126.com>
2017-12-14 17:15:34 +0800

    企业列表界面和设备列表界面以及路由

--- PC/fishery_pc/src/common/nav.js
+++ PC/fishery_pc/src/common/nav.js
@@ -25,7 +25,7 @@ export const getNavData = app => [
       {
         name: '用户管理',
         path: 'userManage',
-        icon: 'form',
+        icon: 'userManagement',
         children: [
           {
             name: '普通用户',
@@ -34,8 +34,8 @@ export const getNavData = app => [
           },
           {
             name: '企业用户',
-            path: 'step-form',
-            component: dynamicWrapper(app, ['form'], () => import('../routes/Forms/StepForm')),
+            path: 'company-user',
+            component: dynamicWrapper(app, ['form'], () => import('../routes/CompanyUser/CompanyUserList')),
             children: [
               {
                 path: 'confirm',
@@ -50,192 +50,32 @@ export const getNavData = app => [
         path: 'basic',
         icon: 'form',
         component: dynamicWrapper(app, ['profile'], () => import('../routes/Profile/BasicProfile')),
-        children: [
-          // {
-          //   name: '基础表单',
-          //   path: 'basic-form',
-          //   component: dynamicWrapper(app, ['form'], () => import('../routes/Forms/BasicForm')),
-          // },
-          // {
-          //   name: '分步表单',
-          //   path: 'step-form',
-          //   component: dynamicWrapper(app, ['form'], () => import('../routes/Forms/StepForm')),
-          //   children: [
-          //     {
-          //       path: 'confirm',
-          //       component: dynamicWrapper(app, ['form'], () => import('../routes/Forms/StepForm/Step2')),
-          //     },
-          //     {
-          //       path: 'result',
-          //       component: dynamicWrapper(app, ['form'], () => import('../routes/Forms/StepForm/Step3')),
-          //     },
-          //   ],
-          // },
-          // {
-          //   name: '高级表单',
-          //   path: 'advanced-form',
-          //   component: dynamicWrapper(app, ['form'], () => import('../routes/Forms/AdvancedForm')),
-          // },
-        ],
+
       },
       {
         name: '账户管理',
         path: 'advanced-form',
         icon: 'form',
         component: dynamicWrapper(app, ['form'], () => import('../routes/Forms/AdvancedForm')),
+
+      },
+      {
+        // name: '设备相关跳转',
+        path: 'equipment',
         children: [
-          // {
-          //   name: '基础表单',
-          //   path: 'basic-form',
-          //   component: dynamicWrapper(app, ['form'], () => import('../routes/Forms/BasicForm')),
-          // },
-          // {
-          //   name: '分步表单',
-          //   path: 'step-form',
-          //   component: dynamicWrapper(app, ['form'], () => import('../routes/Forms/StepForm')),
-          //   children: [
-          //     {
-          //       path: 'confirm',
-          //       component: dynamicWrapper(app, ['form'], () => import('../routes/Forms/StepForm/Step2')),
-          //     },
-          //     {
-          //       path: 'result',
-          //       component: dynamicWrapper(app, ['form'], () => import('../routes/Forms/StepForm/Step3')),
-          //     },
-          //   ],
-          // },
-          // {
-          //   name: '高级表单',
-          //   path: 'advanced-form',
-          //   component: dynamicWrapper(app, ['form'], () => import('../routes/Forms/AdvancedForm')),
-          // },
+          {
+            // name: '设备管理',
+            path: 'equipment-management',
+            component: dynamicWrapper(app, ['form'], () => import('../routes/Equipment/EquipmentManagement')),
+          },
+          {
+            // name: '设备详情',
+            path: 'equipment-detail',
+            component: dynamicWrapper(app, ['form'], () => import('../routes/Equipment/EquipmentDetail')),
+            
+          }
         ],
       },
-      // {
-      //   name: '表单页',
-      //   path: 'form',
-      //   icon: 'form',
-      //   children: [
-      //     {
-      //       name: '基础表单',
-      //       path: 'basic-form',
-      //       component: dynamicWrapper(app, ['form'], () => import('../routes/Forms/BasicForm')),
-      //     },
-      //     {
-      //       name: '分步表单',
-      //       path: 'step-form',
-      //       component: dynamicWrapper(app, ['form'], () => import('../routes/Forms/StepForm')),
-      //       children: [
-      //         {
-      //           path: 'confirm',
-      //           component: dynamicWrapper(app, ['form'], () => import('../routes/Forms/StepForm/Step2')),
-      //         },
-      //         {
-      //           path: 'result',
-      //           component: dynamicWrapper(app, ['form'], () => import('../routes/Forms/StepForm/Step3')),
-      //         },
-      //       ],
-      //     },
-      //     {
-      //       name: '高级表单',
-      //       path: 'advanced-form',
-      //       component: dynamicWrapper(app, ['form'], () => import('../routes/Forms/AdvancedForm')),
-      //     },
-      //   ],
-      // },
-      // {
-      //   name: '列表页',
-      //   path: 'list',
-      //   icon: 'table',
-      //   children: [
-      //     {
-      //       name: '查询表格',
-      //       path: 'table-list',
-      //       component: dynamicWrapper(app, ['rule'], () => import('../routes/List/TableList')),
-      //     },
-      //     {
-      //       name: '标准列表',
-      //       path: 'basic-list',
-      //       component: dynamicWrapper(app, ['list'], () => import('../routes/List/BasicList')),
-      //     },
-      //     {
-      //       name: '卡片列表',
-      //       path: 'card-list',
-      //       component: dynamicWrapper(app, ['list'], () => import('../routes/List/CardList')),
-      //     },
-      //     {
-      //       name: '搜索列表（项目）',
-      //       path: 'cover-card-list',
-      //       component: dynamicWrapper(app, ['list'], () => import('../routes/List/CoverCardList')),
-      //     },
-      //     {
-      //       name: '搜索列表（应用）',
-      //       path: 'filter-card-list',
-      //       component: dynamicWrapper(app, ['list'], () => import('../routes/List/FilterCardList')),
-      //     },
-      //     {
-      //       name: '搜索列表（文章）',
-      //       path: 'search',
-      //       component: dynamicWrapper(app, ['list'], () => import('../routes/List/SearchList')),
-      //     },
-      //   ],
-      // },
-      // {
-      //   name: '详情页',
-      //   path: 'profile',
-      //   icon: 'profile',
-      //   children: [
-      //     {
-      //       name: '基础详情页',
-      //       path: 'basic',
-      //       component: dynamicWrapper(app, ['profile'], () => import('../routes/Profile/BasicProfile')),
-      //     },
-      //     {
-      //       name: '高级详情页',
-      //       path: 'advanced',
-      //       component: dynamicWrapper(app, ['profile'], () => import('../routes/Profile/AdvancedProfile')),
-      //     },
-      //   ],
-      // },
-      // {
-      //   name: '结果',
-      //   path: 'result',
-      //   icon: 'check-circle-o',
-      //   children: [
-      //     {
-      //       name: '成功',
-      //       path: 'success',
-      //       component: dynamicWrapper(app, [], () => import('../routes/Result/Success')),
-      //     },
-      //     {
-      //       name: '失败',
-      //       path: 'fail',
-      //       component: dynamicWrapper(app, [], () => import('../routes/Result/Error')),
-      //     },
-      //   ],
-      // },
-      // {
-      //   name: '异常',
-      //   path: 'exception',
-      //   icon: 'warning',
-      //   children: [
-      //     {
-      //       name: '403',
-      //       path: '403',
-      //       component: dynamicWrapper(app, [], () => import('../routes/Exception/403')),
-      //     },
-      //     {
-      //       name: '404',
-      //       path: '404',
-      //       component: dynamicWrapper(app, [], () => import('../routes/Exception/404')),
-      //     },
-      //     {
-      //       name: '500',
-      //       path: '500',
-      //       component: dynamicWrapper(app, [], () => import('../routes/Exception/500')),
-      //     },
-      //   ],
-      // },
     ],
   },
   {

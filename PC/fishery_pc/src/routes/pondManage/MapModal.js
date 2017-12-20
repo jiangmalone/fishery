dba9d655cf594e640575b1c28a9014374c806556
IDoRemember <1015885915@qq.com>
2017-12-20 09:43:46 +0800

    地图

--- PC/fishery_pc/src/routes/pondManage/MapModal.js
+++ PC/fishery_pc/src/routes/pondManage/MapModal.js
@@ -1,5 +1,5 @@
 import React, { PureComponent } from 'react';
-import { Modal, Input } from 'antd';
+import { Modal, Input, Button } from 'antd';
 import { Map, MouseTool } from 'react-amap';
 
 class MapModal extends PureComponent {
@@ -9,7 +9,23 @@ class MapModal extends PureComponent {
             what: '点击下方按钮开始绘制'
         };
         this.amapEvents = {
-            created: (mapInstance) => {
+            created: (map) => {
+                AMap.plugin(['AMap.Autocomplete', 'AMap.PlaceSearch'], () => {
+                    var autoOptions = {
+                        input: "keyword"//使用联想输入的input的id
+                    };
+                    var autocomplete = new AMap.Autocomplete(autoOptions);
+                    console.log(autocomplete)
+                    var placeSearch = new AMap.PlaceSearch({
+                        city: '',
+                        map: map
+                    })
+                    AMap.event.addListener(autocomplete, "select", function (e) {
+                        //TODO 针对选中的poi实现自己的功能
+                        placeSearch.setCity(e.poi.adcode);
+                        placeSearch.search(e.poi.name)
+                    });
+                })
                 console.log('高德地图 Map 实例创建成功；如果你要亲自对实例进行操作，可以从这里开始。比如：');
             }
         };
@@ -19,7 +35,7 @@ class MapModal extends PureComponent {
                 console.log(tool)
                 this.tool = tool;
             },
-            draw({ obj }) {
+            draw: ({ obj }) => {
                 this.drawWhat(obj);
             }
         }
@@ -44,14 +60,14 @@ class MapModal extends PureComponent {
         });
     }
 
-    drawMarker(){
-        if (this.tool){
-          this.tool.marker();
-          this.setState({
-            what: '准备绘制坐标点'
-          });
+    drawMarker() {
+        if (this.tool) {
+            this.tool.marker();
+            this.setState({
+                what: '准备绘制坐标点'
+            });
         }
-      }
+    }
     render() {
         return (<Modal visible={this.props.visible}
             onOk={this.props.onMapOk}
@@ -62,14 +78,19 @@ class MapModal extends PureComponent {
             cancelText="取消">
             {/* <div id="container" style={{ width: '100%', height: '600px' }}></div> */}
             <div style={{ height: '800px' }}>
-                <Map amapkey={'ae721b47f9c198651a4bfecec901aa8c'} version={'1.4.2'} events={this.amapEvents} >
-                    <MouseTool events={this.markerEvents} />
+                <Map amapkey={'ae721b47f9c198651a4bfecec901aa8c'} version={'1.4.2'}
+                    plugins={this.mapPlugins}
+                    events={this.amapEvents} >
+                    <MouseTool events={this.toolEvents} >
+
+                    </MouseTool>
                 </Map>
             </div>
             <div style={{ position: 'absolute', top: '0' }}>
-                <Input id="keyword" name="keyword" placeholder="请输入关键字：(选定后搜索)" onChange={(e) => { this.setState({ address: e.target.value }) }} />
-                <button onClick={()=>{this.drawMarker()}}>Draw Marker</button>
+                <Input id="keyword" ref="keyword" name="keyword" placeholder="请输入关键字：(选定后搜索)" onChange={(e) => { this.setState({ address: e.target.value }) }} />
+                <Button onClick={() => { this.drawMarker() }}>Draw Marker</Button>
             </div>
+            <div>{this.state.what}</div>
         </Modal>)
     }
 

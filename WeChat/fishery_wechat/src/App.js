d2f39c686d1900e1df80c52befc58d7861a16346
Mrpaker <spectator_w@126.com>
2017-12-26 14:32:50 +0800

    个人信息

--- WeChat/fishery_wechat/src/App.js
+++ WeChat/fishery_wechat/src/App.js
@@ -6,13 +6,14 @@ const Switch = Router.Switch;
 import IndexPage from './routes/IndexPage';
 
 import Login from './routes/Login/Login';
-
+import UserInfo from './routes/Login/UserInfo'
 const App = () => (
     <div style={{height:'100%'}}>
         <Switch>
             <Route exact path="/" component={IndexPage}/>
            
             <Route exact path="/login" component={Login} />
+            <Route exact path="/userInfo" component={UserInfo} />
             {/* <Route component={NotFound}/> */}
         </Switch>
     </div>

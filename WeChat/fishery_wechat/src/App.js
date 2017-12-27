526b43f5c39dbb2563c0186e65496c4d3178a5aa
IDoRemember <1015885915@qq.com>
2017-12-27 17:44:49 +0800

    我的塘口

--- WeChat/fishery_wechat/src/App.js
+++ WeChat/fishery_wechat/src/App.js
@@ -12,26 +12,29 @@ import Alarm from './routes/Alarm/Alarm';
 import PersonalCenter from './routes/PersonalCenter/PersonalCenter';
 import addPond from './routes/Mypond/AddPond/AddPond';
 import addFish from './routes/Mypond/AddPond/AddFish';
+import MyPond from './routes/Mypond/MyPond/MyPond';
 import Main from './routes/Main/Main'
 import AutoOrxygenationSetting from './routes/AutoOxygenationSetting/AutoOxygenationSetting'
 
-const App = (props) =>{
-    return ( true?<div style={{height:'100%'}}>
+const App = (props) => {
+    return (true ? <div style={{ height: '100%' }}>
         <Switch>
-            <Route exact path="/" component={IndexPage}/>
+            <Route exact path="/" component={IndexPage} />
             <Route exact path="/userInfo" component={UserInfo} />
             <Route exact path="/alarm" component={Alarm} />
             <Route path="/center" component={PersonalCenter} />
             <Route path="/addPond" component={addPond} />
             <Route path="/addFish" component={addFish} />
+            <Route path="/MyPond" component={MyPond} />
             <Route exact path="/main" component={Main} />
             <Route exact path="/autoOrxygenationSetting" component={AutoOrxygenationSetting} />
             {/* <Route component={NotFound}/> */}
         </Switch>
-    </div>: <Redirect to={{
+    </div> : <Redirect to={{
         pathname: '/login',
         state: { from: props.location }
-      }}/>
-)};
+    }} />
+    )
+};
 
 module.exports = App;

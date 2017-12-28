507037afb907a4d0dfe3260335bac6e806bf4673
Mrpaker <spectator_w@126.com>
2017-12-28 14:56:02 +0800

    我的设备，静态完成

--- WeChat/fishery_wechat/src/routes/Equipment/MyEquipment.js
+++ WeChat/fishery_wechat/src/routes/Equipment/MyEquipment.js
@@ -1,19 +1,41 @@
 import React from 'react';
 import './myEquipment.less'
-import { Flex, Toast, List, Button, } from 'antd-mobile'
+import { Flex, Toast, List, Button, Modal } from 'antd-mobile'
 import { withRouter } from "react-router-dom";
 import Accordion from '../../components/Accordion';
 import offline from '../../img/equ_link_off.png'
 import online from '../../img/equ_link_on.png'
-
+const alert = Modal.alert;
 
 class MyEquipment extends React.Component {
 
     constructor(props) {
         super(props)
         this.state = {
-
+            isEdit: false
         }
+    }
+
+    edit = () => {
+        this.setState({ isEdit: !this.state.isEdit })
+    }
+
+
+
+    wouldDelete = () => {
+        console.log('wouldDelete');
+        alert('删除', '确定删除该设备吗？', [
+            { text: '取消', onPress: () => console.log('取消') },
+            { text: '确定', onPress: () => this.doDelete() },
+          ])
+    }
+
+    doDelete = () => {
+        console.log('doDelete');
+    }
+
+    addEquipment = () => {
+        console.log('add');
     }
 
     render() {
@@ -21,12 +43,14 @@ class MyEquipment extends React.Component {
             <div className="nav-bar-title">
                 <i className="back" onClick={() => { history.back() }}></i>
                 我的设备
-                <i className="edit"></i>
+                <i className={this.state.isEdit ? 'right-item-none' : "edit"} onClick={this.edit} >{this.state.isEdit && "取消"}</i>
             </div>
             <div className='equipment-type'>
                 <Accordion title='传感器' isShowState={false} isShow={true} >
                     <div className='equipment'>
                         <div className='line' >
+                            {this.state.isEdit && <div className='delete-button' onClick={this.wouldDelete} >
+                            </div>}
                             <div className='name' >
                                 传感器01
                             </div>
@@ -38,6 +62,8 @@ class MyEquipment extends React.Component {
                             </div>
                         </div>
                         <div className='line' >
+                            {this.state.isEdit && <div className='delete-button' onClick={this.wouldDelete} >
+                            </div>}
                             <div className='name' >
                                 传感器02
                             </div>
@@ -53,8 +79,10 @@ class MyEquipment extends React.Component {
                 <Accordion title='控制器' isShowState={false} isShow={true} >
                     <div className='equipment'>
                         <div className='line' >
+                            {this.state.isEdit && <div className='delete-button' onClick={this.wouldDelete} >
+                            </div>}
                             <div className='name' >
-                            控制器01
+                                控制器01
                     </div>
                             <div className='right-imgs'>
                                 <div className='online' >1
@@ -69,8 +97,10 @@ class MyEquipment extends React.Component {
                             </div>
                         </div>
                         <div className='line' >
+                            {this.state.isEdit && <div className='delete-button' onClick={this.wouldDelete} >
+                            </div>}
                             <div className='name' >
-                            控制器02
+                                控制器02
                             </div>
                             <div className='right-imgs'>
                                 <div className='online' >1
@@ -89,8 +119,10 @@ class MyEquipment extends React.Component {
                 <Accordion title='一体机' isShowState={false} isShow={true} >
                     <div className='equipment'>
                         <div className='line' >
+                            {this.state.isEdit && <div className='delete-button' onClick={this.wouldDelete} >
+                            </div>}
                             <div className='name' >
-                            一体机01
+                                一体机01
                             </div>
                             <div className='right-imgs'>
                                 <div className='online' >
@@ -98,8 +130,10 @@ class MyEquipment extends React.Component {
                             </div>
                         </div>
                         <div className='line' >
+                            {this.state.isEdit && <div className='delete-button' onClick={this.wouldDelete} >
+                            </div>}
                             <div className='name' >
-                            一体机02
+                                一体机02
                             </div>
                             <div className='right-imgs'>
                                 <div className='offline' >
@@ -109,7 +143,7 @@ class MyEquipment extends React.Component {
                     </div>
                 </Accordion>
             </div>
-            <div className='add-button'>
+            <div className='add-button' onClick={this.addEquipment} >
             </div>
         </div>
     }

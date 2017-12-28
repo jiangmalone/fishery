6cd4611de2264b1f1c2ad7d96a48da199bee4d57
IDoRemember <1015885915@qq.com>
2017-12-28 11:55:50 +0800

    commit

--- WeChat/fishery_wechat/src/routes/Mypond/MyPond/MyPond.js
+++ WeChat/fishery_wechat/src/routes/Mypond/MyPond/MyPond.js
@@ -1,120 +1,151 @@
 import React from 'react';
-import { List, InputItem, Picker } from 'antd-mobile';
+import { List, InputItem, Picker,ActionSheet  } from 'antd-mobile';
 import NavBar from '../../../components/NavBar';
 import { createForm } from 'rc-form';
 import './Mypond.less'
+let wrapProps;
+const isIPhone = new RegExp('\\biPhone\\b|\\biPod\\b', 'i').test(window.navigator.userAgent);
+if (isIPhone) {
+  wrapProps = {
+    onTouchStart: e => e.preventDefault(),
+  };
+}
+class MyPond extends React.Component {
 
-function MyPond() {
+    constructor(props) {
+        super(props)
+        this.state = {
 
-    return (
-        <div className="body-bac">
-            <div className="nav-bar-title">
-                <i className="back" onClick={() => { history.back() }}></i>
-                我的塘口
-                <i className="edit"></i>
-            </div>
-            <div className="mypond-bac"></div>
-            <div className="mypond-content">
-                <div>
-                    <div className="content-title">小鱼塘</div>
-                    <div>
-                        <span className="content-info">
-                            <i className="content-info-img area-img" />
-                            <span className="content-info-value">
-                                20亩
-                        </span>
-                        </span>
-                        <span className="content-info">
-                            <i className="content-info-img address-img" />
-                            <span className="content-info-value">
-                                南京玄武区鱼儿乐…
-                        </span>
-                        </span>
-                    </div>
-                    <div>
-                        <span className="content-info">
-                            <i className="content-info-img waterDepth-img" />
-                            <span className="content-info-value">
-                                500m
-                        </span>
-                        </span>
-                        <span className="content-info">
-                            <i className="content-info-img poolFish-img" />
-                            <span className="content-info-value">
-                                鲫鱼，草鱼...
-                        </span>
-                        </span>
-                        <span className="content-info">
-                            <i className="content-info-img poolWater-img" />
-                            <span className="content-info-value">
-                                淡水
-                        </span>
-                        </span>
-                        <span className="content-info">
-                            <i className="content-info-img poolThickness-img" />
-                            <span className="content-info-value">
-                                50cm
-                        </span>
-                        </span>
-                    </div>
+        }
+    }
+    showActionSheet = () => {
+        const BUTTONS = ['删除', '取消'];
+        ActionSheet.showActionSheetWithOptions({
+            options: BUTTONS,
+            cancelButtonIndex: BUTTONS.length - 1,
+            destructiveButtonIndex: BUTTONS.length - 2,
+            // title: 'title',
+            message: '您是否确定删除该塘口？',
+            maskClosable: true,
+            'data-seed': 'logId',
+            wrapProps,
+        },
+        (buttonIndex) => {
+            this.setState({ clicked: BUTTONS[buttonIndex] });
+        });
+    }
+    render() {
+        return (
+            <div className="body-bac">
+                <div className="nav-bar-title">
+                    <i className="back" onClick={() => { history.back() }}></i>
+                    我的塘口
+                    <i className="edit"></i>
                 </div>
-                <div className="mypond-delete">
-                    <img src={require('../../../img/btn_remove.png')}/>
-                </div>
-            </div>
-            <div className="mypond-content">
-                <div>
-                    <div className="content-title">小鱼塘</div>
+                <div className="mypond-bac"></div>
+                <div className="mypond-content">
                     <div>
-                        <span className="content-info">
-                            <i className="content-info-img area-img" />
-                            <span className="content-info-value">
-                                20亩
-                        </span>
-                        </span>
-                        <span className="content-info">
-                            <i className="content-info-img address-img" />
-                            <span className="content-info-value">
-                                南京玄武区鱼儿乐…
-                        </span>
-                        </span>
+                        <div className="content-title">小鱼塘</div>
+                        <div>
+                            <span className="content-info">
+                                <i className="content-info-img area-img" />
+                                <span className="content-info-value">
+                                    20亩
+                            </span>
+                            </span>
+                            <span className="content-info">
+                                <i className="content-info-img address-img" />
+                                <span className="content-info-value">
+                                    南京玄武区鱼儿乐…
+                            </span>
+                            </span>
+                        </div>
+                        <div>
+                            <span className="content-info">
+                                <i className="content-info-img waterDepth-img" />
+                                <span className="content-info-value">
+                                    500m
+                            </span>
+                            </span>
+                            <span className="content-info">
+                                <i className="content-info-img poolFish-img" />
+                                <span className="content-info-value">
+                                    鲫鱼，草鱼...
+                            </span>
+                            </span>
+                            <span className="content-info">
+                                <i className="content-info-img poolWater-img" />
+                                <span className="content-info-value">
+                                    淡水
+                            </span>
+                            </span>
+                            <span className="content-info">
+                                <i className="content-info-img poolThickness-img" />
+                                <span className="content-info-value">
+                                    50cm
+                            </span>
+                            </span>
+                        </div>
                     </div>
-                    <div>
-                        <span className="content-info">
-                            <i className="content-info-img waterDepth-img" />
-                            <span className="content-info-value">
-                                500m
-                        </span>
-                        </span>
-                        <span className="content-info">
-                            <i className="content-info-img poolFish-img" />
-                            <span className="content-info-value">
-                                鲫鱼，草鱼...
-                        </span>
-                        </span>
-                        <span className="content-info">
-                            <i className="content-info-img poolWater-img" />
-                            <span className="content-info-value">
-                                淡水
-                        </span>
-                        </span>
-                        <span className="content-info">
-                            <i className="content-info-img poolThickness-img" />
-                            <span className="content-info-value">
-                                50cm
-                        </span>
-                        </span>
+                    <div className="mypond-delete" onClick={this.showActionSheet}>
+                        <img src={require('../../../img/btn_remove.png')} />
                     </div>
                 </div>
-                <div className="mypond-delete">
-                    <img src={require('../../../img/btn_remove.png')}/>
+                <div className="mypond-content">
+                    <div>
+                        <div className="content-title">小鱼塘</div>
+                        <div>
+                            <span className="content-info">
+                                <i className="content-info-img area-img" />
+                                <span className="content-info-value">
+                                    20亩
+                            </span>
+                            </span>
+                            <span className="content-info">
+                                <i className="content-info-img address-img" />
+                                <span className="content-info-value">
+                                    南京玄武区鱼儿乐…
+                            </span>
+                            </span>
+                        </div>
+                        <div>
+                            <span className="content-info">
+                                <i className="content-info-img waterDepth-img" />
+                                <span className="content-info-value">
+                                    500m
+                            </span>
+                            </span>
+                            <span className="content-info">
+                                <i className="content-info-img poolFish-img" />
+                                <span className="content-info-value">
+                                    鲫鱼，草鱼...
+                            </span>
+                            </span>
+                            <span className="content-info">
+                                <i className="content-info-img poolWater-img" />
+                                <span className="content-info-value">
+                                    淡水
+                            </span>
+                            </span>
+                            <span className="content-info">
+                                <i className="content-info-img poolThickness-img" />
+                                <span className="content-info-value">
+                                    50cm
+                            </span>
+                            </span>
+                        </div>
+                    </div>
+                    <div className="mypond-delete">
+                        <img src={require('../../../img/btn_remove.png')} />
+                    </div>
+                </div>
+                <div className="addPond-btn">取消</div>
+                <div className="btn_add">
                 </div>
             </div>
-            <div className="addPond-btn">取消</div>
-            <div className="btn_add">
-            </div>
-        </div>
-    );
+        );
+    }
+
 }
 
 export default MyPond;

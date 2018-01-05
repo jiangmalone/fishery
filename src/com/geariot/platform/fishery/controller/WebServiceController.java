80617f87e043fe05dca723931b02515993c98e5e
mxy940127 <634666730@qq.com>
2018-01-05 17:15:49 +0800

    webservice

---
+++ src/com/geariot/platform/fishery/controller/WebServiceController.java
@@ -0,0 +1,42 @@
+package com.geariot.platform.fishery.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import com.geariot.platform.fishery.utils.HttpRequest;
+
+
+@RestController
+@RequestMapping("/webService")
+public class WebServiceController {
+	
+	private String mapApiKey = "1fb41392b17b0575e03b5374cb7b029f";
+	
+	private String weatherUrl = "http://restapi.amap.com/v3/weather/weatherInfo";
+	
+	private String weatherExtensions ="base";
+	
+	private String ContentType = "application/json";
+	
+	@RequestMapping(value = "/weather" , method = RequestMethod.POST)
+	public String getWeatherTest(String name){
+		Map<String,Object> param = new HashMap<>();
+		param.put("city", name);
+		param.put("extensions",weatherExtensions);
+		param.put("key", mapApiKey);
+		Map<String, Object> head = setWeatherHead();
+		String result = HttpRequest.getCall(weatherUrl, param, head);
+		return result;
+		
+	}
+
+	private Map<String, Object> setWeatherHead() {
+		Map<String,Object> head = new HashMap<String, Object>();
+		head.put("Content-Type", ContentType);
+		return null;
+	}
+	
+	
+}

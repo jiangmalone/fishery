fff2fff0cea580153c09cc32d39160eefd3cdc56
mxy940127 <634666730@qq.com>
2018-01-08 09:32:17 +0800

    commit

--- src/com/geariot/test/fishery/controller/PondControllerTest.java
+++ src/com/geariot/test/fishery/controller/PondControllerTest.java
@@ -12,8 +12,6 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.result.PrintingResultHandler;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;

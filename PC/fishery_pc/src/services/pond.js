3bc814209aa3ab9d86eb7f3c9f29384dbe1af64f
IDoRemember <1015885915@qq.com>
2018-01-10 11:57:11 +0800

    塘口的接口

---
+++ PC/fishery_pc/src/services/pond.js
@@ -0,0 +1,35 @@
+import request from '../utils/request';
+import { stringify } from 'qs';
+
+export async function queryPond(params) {
+    return request(`/api/pond/query?${stringify(params)}`);
+}
+
+export async function addPond(params) {
+    return request(`/api/pond/addPond`, {
+        method: 'POST',
+        body: params
+    });
+}
+
+export async function pondEquipment(params) {
+    return request(`/api/pond/pondEquipment?${stringify(params)}`);
+}
+
+export async function modifyPond(params) {
+    return request('/api/pond/modifyPond', {
+        method: 'POST',
+        body: params
+    });
+}
+
+export async function delPonds(params) {
+    // params = stringify(params).replace(/\%.*?$/,'')
+    // console.log(params)
+    let str = ''
+    for (let item of params.pondIds) {
+        str = 'pondIds=' + item + '&' + str
+    }
+    str = str.slice(0, -1)
+    return request(`/api/pond/delPonds?${str}`)
+}

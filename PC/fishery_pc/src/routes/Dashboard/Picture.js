40aa8ec35819a9b3f3b2eba0fbcf6a252c76ea47
IDoRemember <1015885915@qq.com>
2018-01-24 16:19:09 +0800

    提交

---
+++ PC/fishery_pc/src/routes/Dashboard/Picture.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import styles from './picture.less'
+import { Card } from 'antd';
+export default function Picture() {
+    return <Card>
+            <div className={styles.picture}>
+            </div>
+        </Card>
+}

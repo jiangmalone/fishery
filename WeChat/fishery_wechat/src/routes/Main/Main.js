f4f5951146382f6d2f85571ac74684eb51abc8cd
Mrpaker <spectator_w@126.com>
2018-01-25 17:51:13 +0800

    11

--- WeChat/fishery_wechat/src/routes/Main/Main.js
+++ WeChat/fishery_wechat/src/routes/Main/Main.js
@@ -236,7 +236,7 @@ class Main extends React.Component {
                 <div className='name' >
                     增氧机（{aio.way}路）
                 </div>
-                <button className='auto-button do-auto' onClick={() => this.showActionSheet(aio.device_sn, aio.way)} >自动</button>
+                <button className='auto-button do-auto' onClick={() => this.showActionSheet(aio.device_sn, aio.way)} >定时</button>
                 <Switch
                     nanme='watertem'
                     checked={this.state.waterCheck1}

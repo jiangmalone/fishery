100bd4ff7074b430a4c3b7d39b670f4b5a35d05c
liying <841114322@qq.com>
2018-01-29 15:09:54 +0800

    111

--- src/com/geariot/platform/fishery/socket/CMDUtils.java
+++ src/com/geariot/platform/fishery/socket/CMDUtils.java
@@ -599,17 +599,17 @@ public class CMDUtils {
 		}
 		
 		
-		long start=System.currentTimeMillis();
+		//long start=System.currentTimeMillis();
 		WXUser wxuser=new WXUser();
 		wxuser=service.findWXUserById(relation);
 		BrokenMSG bs=new BrokenMSG();
-		System.out.println(bs.getMSG());
-		System.out.println(wxuser.getOpenId());
+		//System.out.println(bs.getMSG());
+		//System.out.println(wxuser.getOpenId());
 		WechatSendMessageUtils.sendWechatMessages(bs.getMSG(),wxuser.getOpenId());
 		//WechatTemplateMessage.sendBrokenMSG(bs.getMSG(),wxuser.getOpenId());//把所有故障信息拼接完毕推送给前台
 		bs.clear(); 
-		long end=System.currentTimeMillis();
-		System.out.println(end-start);
+		//long end=System.currentTimeMillis();
+		//System.out.println(end-start);
 		
 	}
 	

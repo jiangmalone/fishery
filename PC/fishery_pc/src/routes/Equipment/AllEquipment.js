4f8ff54d2a7af3fd95118fab9fd46f12a780bc39
IDoRemember <1015885915@qq.com>
2018-01-30 11:23:37 +0800

    所有者需求更改

--- PC/fishery_pc/src/routes/Equipment/AllEquipment.js
+++ PC/fishery_pc/src/routes/Equipment/AllEquipment.js
@@ -4,6 +4,7 @@ import { connect } from 'dva';
 import PageHeaderLayout from '../../layouts/PageHeaderLayout';
 import { Table, Card, Row, Col, Input, Button } from 'antd'
 import { Link } from 'react-router-dom'
+import index from '../../../node_modules/_antd@3.0.1@antd/lib/checkbox';
 
 @connect(state => ({
     list: state.allequipment.list,
@@ -31,7 +32,7 @@ class AllEquipmentQuery extends PureComponent {
             obj.device_sn = this.state.device_sn
         } 
         if(this.state.name){
-            obj.companyName = this.state.name
+            obj.userName = this.state.name
         } 
         obj.page = page;
         obj.number = 10;
@@ -75,8 +76,17 @@ class AllEquipmentQuery extends PureComponent {
                 dataIndex: 'name',
             },
             {
-                title: '所属企业',
-                dataIndex: 'companyName'
+                title: '所属者',
+                dataIndex: 'userName',
+                render:(text,record,index)=>{
+                    if(record.relation!=='0'&&record.relation.slice(0,2)=='CO'){
+                        return <Link to={`/userManage/company-user/${record.relation.slice(2)}/${record.relation}`}>{record.userName}</Link>
+                    } else if (record.relation!=='0'&&record.relation.slice(0,2)=='WX') {
+                        return <Link to={`/userManage/common-user/${record.relation}`}>{record.userName}</Link>
+                    } else {
+                        return <span>{text}</span>
+                    }
+                }
             },
             {
                 title: '设备状态',
@@ -103,7 +113,7 @@ class AllEquipmentQuery extends PureComponent {
                                 device_sn: e.target.value
                             })
                         }} /></Col>
-                        <Col span={8}>企业名称：<Input style={{ width: 200 }} onChange={(e) => {
+                        <Col span={8}>所有者名称：<Input style={{ width: 200 }} onChange={(e) => {
                             this.setState({
                                 name: e.target.value
                             })

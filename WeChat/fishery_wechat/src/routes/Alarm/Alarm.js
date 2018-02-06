38f75c4e9e20e0cfade28ba2d62b4ca70e483cbb
IDoRemember <1015885915@qq.com>
2018-02-06 10:57:54 +0800

    告警

--- WeChat/fishery_wechat/src/routes/Alarm/Alarm.js
+++ WeChat/fishery_wechat/src/routes/Alarm/Alarm.js
@@ -10,32 +10,38 @@ class Alarm extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            alarms:[]
+            alarms: []
         }
     }
     componentDidMount() {
         queryAlarm({
-            openId:window.localStorage.getItem('openid')
-        }).then((res)=>{
+            openId: window.localStorage.getItem('openid')
+        }).then((res) => {
             console.log(res)
-            if(res.data.code == 0) {
+            if (res.data.code == 0) {
                 this.setState({
-                   alarms:res.data.alarmMessageList 
+                    alarms: res.data.dataAlarm
                 })
             }
-        }).catch((error)=>{
+        }).catch((error) => {
             console.log(error)
         })
     }
     render() {
-        let alarmInfos =  this.state.alarms.map((Item,index)=>{
-            return   <div className="alarm-list">
-            <div className="list-title">{Item.pond.name}-</div>
-            <div className="list-info">
-                <div className="info-state"><div className="info-circle"></div>溶氧数据异常</div>
-                <div className="info-span">溶氧数值过低，鱼儿没法生存啦~</div>
+        let alarmInfos = this.state.alarms.map((Item, index) => {
+            let alarmTitle = ''
+            switch (Item.alarmType) {
+                case 0: alarmTitle = '溶氧值'; break;
+                case 1: alarmTitle = '温度'; break;
+                case 2: alarmTitle = 'PH'; break;
+            }
+            return <div className="alarm-list">
+                <div className="list-title">{Item.pondName}-{Item.deviceName}</div>
+                <div className="list-info">
+                    <div className="info-state"><div className="info-circle"></div>{alarmTitle}数据异常</div>
+                    <div className="info-span">{Item.message}</div>
+                </div>
             </div>
-        </div>
         })
         return (
             <div className="alarm-box">

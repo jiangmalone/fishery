cbb7cb1c42c66f9457d91439415d2a4c493cfbca
Mrpaker <spectator_w@126.com>
2018-03-02 09:53:37 +0800

    1

--- WeChat/fishery_wechat/src/routes/AutoOxygenationSetting/AutoOxygenationSetting.js
+++ WeChat/fishery_wechat/src/routes/AutoOxygenationSetting/AutoOxygenationSetting.js
@@ -105,22 +105,7 @@ class AutoOxygenationSetting extends React.Component {
                 // alert('Validation failed');
             }
         });
-    }
-    getTaskTime = (strDate) => {  
-        console.log("原始时间格式："+strDate);  
-        var date = new Date(strDate);  
-        var y = date.getFullYear();   
-        var m = date.getMonth() + 1;    
-        m = m < 10 ? ('0' + m) : m;    
-        var d = date.getDate();    
-        d = d < 10 ? ('0' + d) : d;    
-        var h = date.getHours();    
-        var minute = date.getMinutes();    
-        minute = minute < 10 ? ('0' + minute) : minute;  
-        var str = y+"-"+m+"-"+d+" "+h+":"+minute;  
-        console.log("转换时间格式："+str);  
-        return str;  
-    };  
+    } 
     
     queryAeratorData = () => {
         queryAeratorData({

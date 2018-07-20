65113d1de747e09c311ec665915f2562d4b0a59a
pyt666 <2630451673@qq.com>
2018-07-20 10:33:07 +0800

    修复缺相断电状态显示异常

--- src/com/geariot/platform/fishery/service/UserService.java
+++ src/com/geariot/platform/fishery/service/UserService.java
@@ -360,9 +360,8 @@ public class UserService {
 					//获得设备离线/在线状态
 					GetDevicesStatus api = new GetDevicesStatus(controller.getDevice_sn(),key);
 			        BasicResponse<DevicesStatusList> response = api.executeApi();
-			        
-			        
-			        System.out.println(response.getJson());
+			        logger.debug("获取首页控制器状态："+response.getJson());
+	
 			        if(response.errno == 0) {
 			        	controllerMap.put("online", response.data.getDevices().get(0).getIsonline());
 			        	//获得控制器最新数据
@@ -375,11 +374,12 @@ public class UserService {
 				        		for(int i=0;i<datastreamsList.size();i++) {
 						        	controllerDataMap.put(datastreamsList.get(i).getId(), datastreamsList.get(i).getValue());
 						        }        	
-						        controllerMap.put("data", controllerDataMap);
-						        if(controllerMap.get("PF") !=null) {
-						        	int PF = (int) controllerMap.get("PF");
-							        int DP = (int) controllerMap.get("DP"+controller.getPort());
-							        if(PF == 1) {
+						        controllerMap.put("data", controllerDataMap);						      
+						        if(controllerDataMap.get("PF") !=null) {						      						        	
+						        	String  PF = (String) controllerDataMap.get("PF");
+							        String DP = (String) controllerDataMap.get("DP"+controller.getPort());
+							        logger.debug("断电1或正常0："+PF);
+							        if(PF.equals("1")) {
 							        	controller.setStatus(2);
 							        	WechatSendMessageUtils.sendWechatVoltageMessages("断电报警", wxUser.getOpenId(), controller.getDevice_sn());
 							        	
@@ -393,7 +393,8 @@ public class UserService {
 										}
 							        	
 							        }else {
-							        	if(DP==1) {
+							        	if(DP.equals("1")) {
+							        		logger.debug("缺相1或正常0："+DP);
 							        		controller.setStatus(3);
 							        		WechatSendMessageUtils.sendWechatOxyAlarmMessages("缺相报警", wxUser.getOpenId(), controller.getDevice_sn());
 							        		String json = "{\"deviceName\":\"" + controller.getName() + "\",\"way\":" + controller.getPort() + "}";

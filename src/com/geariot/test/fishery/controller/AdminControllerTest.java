70072990764b56127669291002dc898b5420f335
pyt <2630451673@qq.com>
2018-10-29 10:59:58 +0800

    test

--- src/com/geariot/test/fishery/controller/AdminControllerTest.java
+++ src/com/geariot/test/fishery/controller/AdminControllerTest.java
@@ -1,8 +1,4 @@
-/**
- * 
- */
 package com.geariot.test.fishery.controller;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
